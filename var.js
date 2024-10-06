07805dd4543efbeb81e5b68b94694cd07edcb965
VIJAY <148955205+VIJAYLEO8585@users.noreply.github.com>
2024-10-06 13:43:29 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,8 @@
+// Task 1: creating variables for Product Details(Primitive data types)
+let productName = "Wireless Mouse";    // String
+let productPrice = 29.99;              // Number (Float)
+let productAvailable = true;           // Boolean
+let productRating = 4.5;               // Number (Float)
+
+// Task 2: creating Array for Product Categories(non-primitive)
+let productCategories = ["Electronics", "Computer Accessories", "Wireless Devices"];
